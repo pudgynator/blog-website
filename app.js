2df8a7b2344da9ea86fba8912c2fd8ba01988bc4
Maria <dobrydnyk.m@gmail.com>
2026-05-01 16:51:29 +0300

    Structured project. Add Express Router and MVC

--- app.js
+++ app.js
@@ -1,89 +1,42 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+
 const { render } = require("ejs");
-
-
+const blogRoutes = require("./routes/blogRoutes");
 
 const dbiURI = process.env.MONGO_URI;
 
-
-mongoose.connect(dbiURI)
-    .then((result) => app.listen(3000, () => {
-        console.log("Server is listening on port 3000");
-    }))
-    .catch((err) => console.log(err));
+mongoose
+  .connect(dbiURI)
+  .then((result) =>
+    app.listen(3000, () => {
+      console.log("Server is listening on port 3000");
+    })
+  )
+  .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=> {
-    res.redirect('/blogs');
-    //res.render('index', {title: 'Home', blogs});
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+  //res.render('index', {title: 'Home', blogs});
 });
 
-app.get("/about", (req, res)=> {
-    res.render('about', {title: "About"});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
 //blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort( {createdAt: -1} )
-        .then((result) => {
-            res.render('index', { title: 'All Blogs', blogs: result });
-        })
-        .catch((err) => console.log(err));
-});
-
-//post handler
-app.post('/blogs', (req, res) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs');
-        })
-        .catch((err) => console.log(err));
-});
-
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-})
-
-app.get('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-        .then((result) => {
-            if (!result) {
-                return res.status(404).render('404', { title: 'Blog not found' });
-            }
-            res.render('details', { title: 'Blog details', blog: result });
-        })
-        .catch((err) => {
-            console.log(err)
-            res.status(500).send('Server error');
-        });
-});
-
-
-app.delete('/blogs/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: '/blogs' });
-         })
-        .catch((err) => console.log(err));
-}); 
+app.use("/blogs", blogRoutes);
 
 //404 page
 app.use((req, res) => {
-    res.status(404).render('404', {title: "404"});
+  res.status(404).render("404", { title: "404" });
 });
